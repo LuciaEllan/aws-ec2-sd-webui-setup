34836bc014da50ee2855233875324c3c5afe1831
LuciaEllan <lucia.ellan@gmail.com>
2023-09-09 09:22:51 +0900

    Update and rename setup_instance_store to setup_instance_store.sh

--- setup_instance_store.sh
+++ setup_instance_store.sh
@@ -3,6 +3,11 @@
 # main script from https://towardsdatascience.com/create-your-own-stable-diffusion-ui-on-aws-in-minutes-35480dfcde6a
 # changed a bit to load latest AUTOMATIC1111's web UI and not start it automatically
 
+# disable the restart dialogue and install several packages
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo apt-get update
+sudo apt install wget git python3 python3-venv build-essential net-tools awscli -y
+
 # make filesystem for instance store
 sudo mkfs -t ext4 /dev/nvme1n1
 
@@ -17,23 +22,22 @@ sudo chown ubuntu:ubuntu /mnt/ssd
 sudo mkdir /tmp/gradio
 sudo chown ubuntu:ubuntu /tmp/gradio
 
-# disable the restart dialogue and install several packages
-sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
-sudo apt-get update -y
-sudo apt install wget git python3 python3-venv build-essential net-tools awscli -y
-
-# move to the instance store
-cd /mnt/ssd/
-
-# change ownership of the web UI so that a regular user can start the server
-sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
-
 # download AUTOMATIC1111's web UI
+cd /mnt/ssd/
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 # install CUDA (from https://developer.nvidia.com/cuda-downloads)
 wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
 sudo sh cuda_12.2.0_535.54.03_linux.run --silent
 
-cd ./stable-diffusion-webui/
-nohup bash webui.sh --listen &
+# change ownership of the web UI so that a regular user can start the server
+sudo chown -R ubuntu:ubuntu stable-diffusion-webui/
+
+# download some models from CivitAI
+cd stable-diffusion-webui/models/Stable-diffusion/
+wget https://civitai.com/api/download/models/130720 --content-disposition
+wget https://civitai.com/api/download/models/150998 --content-disposition
+wget https://civitai.com/api/download/models/150441 --content-disposition
+wget https://civitai.com/api/download/models/153924 --content-disposition
+wget https://civitai.com/api/download/models/144229 --content-disposition
+wget https://civitai.com/api/download/models/148259 --content-disposition
